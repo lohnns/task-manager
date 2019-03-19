9f64b40ae2f2231f97aab4ee7c60c3f5ba240779
lohnns <lohnns.dev@gmail.com>
2019-03-18 21:35:16 -0400

    Initializing spring-boot

---
+++ src/main/java/io/lohnns/taskmanager/TaskManagerApplication.java
@@ -0,0 +1,13 @@
+package io.lohnns.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerApplication.class, args);
+    }
+
+}
